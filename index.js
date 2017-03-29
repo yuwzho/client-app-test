61172504683592a8f34e6653e822ccdb3509bf2a
Yuwei Zhou <yuwzho@microsoft.com>
2017-03-29 13:23:23 +0800

    Update

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ var client;
 var consumerGroup;
 var deviceId;
 
-describe('Test client application', function() {
-  before(function() {
+describe('Test client application', function () {
+  before(function () {
+    this.timeout(60000);
+
     // start to run the client
     var deviceConnectionString = credential.getDeviceConnectionString();
     sample.run(deviceConnectionString);
@@ -19,24 +21,63 @@ describe('Test client application', function() {
     deviceId = credential.getDeviceId();
   });
 
-  after(function() {
+  after(function () {
     sample.stop();
     client.stopReadMessage();
   });
-  
+
   it('get iot hub device-to-cloud message', function (done) {
-    this.timeout(60000);
+    this.timeout(30000);
+    this.slow(15000);
     var start;
     var count = 0;
-    client.startReadMessage(consumerGroup, deviceId, function(message, time) {
-      console.log(JSON.stringify(message));
+    client.startReadMessage(consumerGroup, deviceId, function (message, time) {
       start = start || message.messageId;
       assert.equal(start + count, message.messageId);
       count++;
       // read 10 message and stop
-      if(count === 10) {
+      if (count === 5) {
+        client.stopReadMessage();
         done();
       }
     });
   });
+
+  it('send iot hub device-method to stop', function (done) {
+    var timeoutInSeconds = 10;
+    this.timeout(timeoutInSeconds * 2 * 1000);
+    this.slow(1000);
+    client.deviceMethod(deviceId, 'stop', null, timeoutInSeconds, function (err, result) {
+      if (err) {
+        done(err);
+      } else {
+        assert.equal(200, result.status);
+        assert.equal('Successfully invoke device method', result.payload);
+        done();
+      }
+    });
+  });
+
+  it('send iot hub device-method to start', function (done) {
+    var timeoutInSeconds = 10;
+    this.timeout(timeoutInSeconds * 2 * 1000);
+    this.slow(5000);
+    client.deviceMethod(deviceId, 'start', null, timeoutInSeconds, function (err, result) {
+      if (err) {
+        done(err);
+      } else {
+        assert.equal(200, result.status);
+        assert.equal('Successfully invoke device method', result.payload);
+        client.stopReadMessage();
+        setTimeout(function () {
+          client.startReadMessage(consumerGroup, deviceId, function (message, time) {
+            client.stopReadMessage();
+            done();
+          });
+        }, 500);
+      }
+    });
+  });
+
+
 });
